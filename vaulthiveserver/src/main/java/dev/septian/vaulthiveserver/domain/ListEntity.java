761a245d3189218afa99cead7e7ecdefae345467
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-05-28 00:23:51 +0700

    Update List and ListGame entity

--- vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/domain/ListEntity.java
+++ vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/domain/ListEntity.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -13,22 +14,38 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "lists")
 public class ListEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String description;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     @JoinColumn(name = "list_id", referencedColumnName = "id")
     private Set<ListGameEntity> listGames = new HashSet<>();
+
+    public void add(ListGameEntity item) {
+        if (item != null) {
+            if (listGames == null) {
+                listGames = new HashSet<>();
+            }
+            listGames.add(item);
+        }
+    }
 }
--- vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/domain/ListGameEntity.java
+++ vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/domain/ListGameEntity.java
@@ -6,9 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "list_games")
 public class ListGameEntity {
@@ -17,5 +23,5 @@ public class ListGameEntity {
     private int id;
 
     @Column(name = "game_id")
-    private int game;
+    private int gameId;
 }
