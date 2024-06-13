b2aaeff8dab585e2fdac905fc297af3cd1796fbe
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-06-13 21:21:31 +0700

    Server: enable CORS % update application.yml

--- vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/configurations/WebConfig.java
+++ vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/configurations/WebConfig.java
@@ -3,6 +3,8 @@ package dev.septian.vaulthiveserver.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
@@ -12,6 +14,16 @@ public class WebConfig {
         return builder
             .baseUrl("https://api.rawg.io/api")
             .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
     
 }
