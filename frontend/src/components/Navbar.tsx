2ec709d0393ae5e5f3c76cadc828b842dcdfb8b5
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-06-17 13:19:38 +0700

    Frontend: Update Navbar component to handle authentication state and logout functionality

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { NAVIGATION } from "../constants";
+import { useAuth } from "../context/useAuth";
 
 const Navbar = () => {
+    const { isLoggedIn, logout } = useAuth();
+
     return (
         <nav className="w-full shadow-sm bg-transparent">
             <div className="flex items-center h-16 px-24">
@@ -19,20 +22,31 @@ const Navbar = () => {
                         </Link>
                     ))}
                 </div>
-                <div className="flex items-center gap-4">
-                    <Link
-                        to="/login"
-                        className="text-base font-medium uppercase transition-colors duration-200 hover:text-white"
-                    >
-                        sign in
-                    </Link>
-                    <Link
-                        to="/register"
-                        className="text-base font-medium uppercase transition-colors duration-200 hover:text-white"
-                    >
-                        create account
-                    </Link>
-                </div>
+                {!isLoggedIn() ? (
+                    <div className="flex items-center gap-4">
+                        <Link
+                            to="/login"
+                            className="text-base font-medium uppercase transition-colors duration-200 hover:text-white"
+                        >
+                            sign in
+                        </Link>
+                        <Link
+                            to="/register"
+                            className="text-base font-medium uppercase transition-colors duration-200 hover:text-white"
+                        >
+                            create account
+                        </Link>
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-4">
+                        <button
+                            onClick={logout}
+                            className="text-base font-medium uppercase transition-colors duration-200 hover:text-white"
+                        >
+                            logout
+                        </button>
+                    </div>
+                )}
             </div>
         </nav>
     );
