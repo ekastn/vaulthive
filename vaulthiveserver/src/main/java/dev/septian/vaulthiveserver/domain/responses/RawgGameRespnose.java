42a0f965fd58e5ae456fb414ffa3d8b41dee351b
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-06-18 14:43:45 +0700

    Server: add RawgGameResponse POJO

---
+++ vaulthiveserver/src/main/java/dev/septian/vaulthiveserver/domain/responses/RawgGameRespnose.java
@@ -0,0 +1,56 @@
+package dev.septian.vaulthiveserver.domain.responses;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import dev.septian.vaulthiveserver.domain.dtos.DeveloperDto;
+import dev.septian.vaulthiveserver.domain.dtos.GenreDto;
+import dev.septian.vaulthiveserver.domain.dtos.PlatformDto;
+import dev.septian.vaulthiveserver.domain.dtos.PublisherDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RawgGameRespnose {
+    
+    private int id;
+
+    private String name;
+
+    private String slug;
+
+    @JsonAlias("description_raw")
+    @JsonProperty("description")
+    private String description;
+
+    private String released;
+
+    private float rating;
+
+    private Set<DeveloperDto> developers;
+
+    private Set<PublisherDto> publishers;
+
+    private Set<GenreDto> genres;
+
+    @JsonAlias("platforms.platform")
+    @JsonProperty("platforms")
+    private Set<PlatformWrapper> platforms;
+
+    @Data
+    public static class PlatformWrapper {
+        private PlatformDto platform;
+    }
+
+    public Set<PlatformDto> getPlatforms() {
+        return platforms.stream().map(PlatformWrapper::getPlatform).collect(java.util.stream.Collectors.toSet());
+    }
+
+}
