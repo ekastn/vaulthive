53f683105d4d3b8d3bcc5969c272258ed88fb583
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-06-19 11:37:06 +0700

    Frontend: add gameDetail type

--- frontend/src/types/game.d.ts
+++ frontend/src/types/game.d.ts
@@ -1,10 +1,23 @@
+type Common = {
+    id: number;
+    name: string;
+    slug: string;
+};
+
+type Screenshot = {
+    id: number;
+    image: string;
+    width: number;
+    height: number;
+};
+
 declare type GameSearch = {
     id: number;
     name: string;
     slug: string;
     released: Date;
     imageUrl: string;
-}
+};
 
 declare type Game = {
     id: number;
@@ -14,4 +27,19 @@ declare type Game = {
     released: string;
     imageUrl: string;
     rating: number;
-}
+};
+
+declare type GameDetails = {
+    id: number;
+    name: string;
+    slug: string;
+    description: string;
+    released: string;
+    rating: number;
+    imageUrl: string;
+    developers: Common[];
+    publishers: Common[];
+    genres: Common[];
+    platforms: Common[];
+    screenshots: Screenshot[];
+};
