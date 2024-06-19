1ce9df24f5911c5593fc62c7bb6a813662e599ac
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-06-19 17:21:53 +0700

    Frontend: update game page

--- frontend/src/pages/Game/index.tsx
+++ frontend/src/pages/Game/index.tsx
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 import { getGameDetailsApi } from "../../services/gameService";
 import Spinner from "../../components/Spinner";
-import { FcRating } from "react-icons/fc";
 import { FaStar } from "react-icons/fa";
 import { MdAddCard } from "react-icons/md";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useState } from "react";
 
 const Game = () => {
+    const [imageIndex, setImageIndex] = useState(0);
     const { state } = useLocation();
 
     const {
@@ -18,6 +20,9 @@ const Game = () => {
         queryFn: async () => getGameDetailsApi(state.game.id),
     });
 
+    const nextImage = () => setImageIndex((prev) => (prev === (game?.screenshots.length ?? 0) ? 0 : prev + 1));
+    const prevImage = () => setImageIndex((prev) => (prev === 0 ? (game?.screenshots.length ?? 0) : prev - 1));
+
     if (isLoading) {
         return <Spinner className="mt-24" />;
     }
@@ -25,8 +30,6 @@ const Game = () => {
     if (error) {
         console.log(error);
     }
-
-    console.log(game);
 
     return (
         <main className="px-24 py-4 flex flex-col gap-4 h-[calc(100vh-150px)]">
@@ -37,7 +40,10 @@ const Game = () => {
                         <h3 className="text-sm uppercase font-extralight">Rating</h3>
                         <div className="flex items-center gap-1">
                             <FaStar className="text-2xl" />
-                            <p>{game?.rating}</p>
+                            <p>
+                                {game?.rating}
+                                <span className="text-sm font-extralight">/5</span>
+                            </p>
                         </div>
                     </div>
                     <div className="flex flex-col items-center px-2 py-1">
@@ -47,7 +53,26 @@ const Game = () => {
                 </div>
             </div>
             <div className="grid gap-7 grid-cols-[auto_max(30vw,300px)] h-full">
-                <img src={game?.imageUrl} alt={game?.name} className="object-cover h-full rounded-lg" />
+                <div className="w-full carousel">
+                    <div className="relative w-full carousel-item">
+                        <img src={game?.imageUrl} className={`w-full object-cover ${imageIndex === 0 ? "block" : "hidden"}`} />
+                        {game?.screenshots.map((screenshot, index) => (
+                            <img
+                                key={index + 1}
+                                src={screenshot.image}
+                                className={`w-full object-cover ${index + 1 === imageIndex ? "block" : "hidden"}`}
+                            />
+                        ))}
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                         <button onClick={prevImage} className="p-1 text-gray-600 rounded-full shadow bg-black/20 hover:text-gray-400 hover:bg-black/55" >
+                            <IoIosArrowBack className="text-4xl cursor-pointer" />
+                        </button>
+                         <button onClick={nextImage} className="p-1 text-gray-600 rounded-full shadow bg-black/20 hover:text-gray-400 hover:bg-black/55" >
+                            <IoIosArrowForward className="text-4xl cursor-pointer" />
+                        </button>
+                        </div>
+                    </div>
+                </div>
                 <div className="flex flex-col overflow-y-hidden">
                     <div className="h-full p-2 overflow-y-scroll space-y-1 text-[10pt] font-light">
                         <h2 className="mb-1 text-xl font-bold">Description</h2>
@@ -55,29 +80,31 @@ const Game = () => {
                     </div>
                     <div className="w-full px-4 py-4 space-y-1 text-sm font-light text-gray-300 bg-gray-900">
                         <div>
-                            Genre: {game?.genres.map( (genre, index, self) => `${genre.name}${index < self.length - 1 ? ", " : ""}`)}
+                            Genre:{" "}
+                            {game?.genres.map(
+                                (genre, index, self) => `${genre.name}${index < self.length - 1 ? ", " : ""}`,
+                            )}
                         </div>
                         <div>
-                            Platform: {game?.platforms.map( (platform, index, self) => `${platform.name}${index < self.length - 1 ? ", " : ""}`)}
+                            Platform:{" "}
+                            {game?.platforms.map(
+                                (platform, index, self) => `${platform.name}${index < self.length - 1 ? ", " : ""}`,
+                            )}
                         </div>
                         <div>
-                            Developer: {game?.developers.map( (developer, index, self) => `${developer.name}${index < self.length - 1 ? ", " : ""}`)}
+                            Developer:{" "}
+                            {game?.developers.map(
+                                (developer, index, self) => `${developer.name}${index < self.length - 1 ? ", " : ""}`,
+                            )}
                         </div>
                         <div>
-                            Publisher: {game?.publishers.map((publisher, index, self) => `${publisher.name}${index < self.length - 1 ? ", " : ""}`)}
+                            Publisher:{" "}
+                            {game?.publishers.map(
+                                (publisher, index, self) => `${publisher.name}${index < self.length - 1 ? ", " : ""}`,
+                            )}
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="grid grid-cols-3 gap-4">
-                {game?.screenshots.map((screenshot) => (
-                    <img
-                        key={screenshot.id}
-                        src={screenshot.image}
-                        alt={game.name}
-                        className="object-cover h-[200px] rounded-lg"
-                    />
-                ))}
             </div>
         </main>
     );
