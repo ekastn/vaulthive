f65167f5da5f5f55b5bfa0a5e4fc6229c0df549e
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-06-23 13:45:42 +0700

    Server: add constants

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -26,6 +26,94 @@ export const FEATURES = [
     },
     {
         title: "Rate and Review",
-        description: "Share your thoughts and experiences by rating and reviewing games, and see what others have to say.",
+        description:
+            "Share your thoughts and experiences by rating and reviewing games, and see what others have to say.",
     },
 ];
+
+export const PLATFORMS = [
+    {
+        id: 1,
+        name: "PC",
+        slug: "pc",
+    },
+    {
+        id: 2,
+        name: "PlayStation",
+        slug: "playstation",
+    },
+    {
+        id: 3,
+        name: "Xbox",
+        slug: "xbox",
+    },
+    {
+        id: 4,
+        name: "IOS",
+        slug: "ios",
+    },
+    {
+        id: 8,
+        name: "Android",
+        slug: "android",
+    },
+    {
+        id: 7,
+        name: "Nintendo",
+        slug: "nintendo",
+    },
+];
+
+export const GENRES = [
+    {
+        id: crypto.randomUUID(),
+        name: "Action",
+        slug: "action",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Adventure",
+        slug: "adventure",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "RPG",
+        slug: "roe-playing-game",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Simulation",
+        slug: "simulation",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Shooter",
+        slug: "shooter",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Sports",
+        slug: "sports",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Puzzle",
+        slug: "puzzle",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Racing",
+        slug: "racing",
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Fighting",
+        slug: "fighting",
+    },
+];
+
+export const FILTER_TYPES = {
+    PLATFORM: "platform",
+    GENRE: "genre",
+    NAME: "search",
+};
